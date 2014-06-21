c8d2e7e15645042298dab96d931d9f96f742d01a
Roman Snitko <roman.snitko@gmail.com>
2014-06-22 01:44:45 +0400

    Proper documentation in comments for Subscriber mixin

--- lib/src/publisher.dart
+++ lib/src/publisher.dart
@@ -1,4 +1,4 @@
-part of observable;
+part of observable_roles;
 
 abstract class Publisher {
 
--- lib/src/subscriber.dart
+++ lib/src/subscriber.dart
@@ -1,8 +1,35 @@
-part of observable;
+part of observable_roles;
 
+/**
+ * Watches publishers for events they emit, then reacts to each event by invoking event handlers.
+ *
+ * Subscriber objects are added into publisher's `Pubslisher.observing_subscriber` List.
+ * Every time such a publisher publishes an event it makes sure our subscriber
+ * is notified - that is achieved by calling the `captureEvent()` method.
+ *
+ * `captureEvent()` in turn, doesn't autmoatically execute the event handler,
+ * but simply adds the event to queue. This is because we want to process events
+ * one by one and the order of executing the handlers may matter. After the event
+ * is added into the queue, it calls `_releaseQueuedEvents()` which invokes event
+ * handlers for each event in the queue until it ends. Thus, it may happen so that
+ * some calls to `_releaseQueuedEvents()` may actually be for nothing, since event B
+ * previously added to the queue was released by the call made to `_releaseQueuedEvents()`
+ * while event A was captured. This an unlikely situation, though.
+ *
+ * More importantly, each subscriber has a listening lock. When it's on (set to `true`),
+ * events are captured (added to the queue), but handlers for them are not yet invoked.
+ * as soon as the value of the listening lock changes to `false`, events start being released
+ * by calling `_releaseQueuedEvents()`. This may be very useful in certain cases when
+ * you have to make sure event handlers are invoked only when an object reaches a certain state,
+ * but no earlier (example: process clicks only when the page is loaded).
+ */
 abstract class Subscriber {
 
-  Map  event_handlers = {}; 
+  /// A map of functions serving as event handlers.
+  ///
+  /// Those are invoked each time an event with the specified key is fired
+  /// by one of the publishers this subscriber watches.
+  Map event_handlers = {}; 
 
   // Whenever this one is set to true, we put events in a waiting queue
   // will put events in a queue and wait until it's true again -
@@ -15,28 +42,28 @@ abstract class Subscriber {
     if(lock == false) _releaseQueuedEvents();
   } 
 
-  // Keeps events that are currently on hold because of the publishers_listening_lock 
+  /// Keeps events that are currently on hold because of the publishers listening_lock 
   List events_queue = [];
 
-  /* Captures events but does not execute it. Instead, adds event to a queue,
-     then unless listening_lock is on, releases all of the queued events with
-     _releaseQueuedEvents(). Really, if listening lock is off, it may be just
-     this one event in the queue, but we have to be sure.
-  */
+  /**
+   * Captures events but does not execute it.
+   * 
+   * Instead, adds event to a queue,
+   * then unless listening_lock is on, releases all of the queued events with
+   * _releaseQueuedEvents(). Really, if listening lock is off, it may be just
+   * this one event in the queue, but we have to be sure.
+   */
   captureEvent(e, [data=null]) {
     events_queue.add({ 'name': e, 'data': data});
     if(listening_lock == false) _releaseQueuedEvents();
   }
 
-  print_event(text, e, data) {
-    if(e == 'exchange.orderbook_position_updated')
-      print("${text}: ${data}");
-  }
-
-  /* Releases all the events that were previously queued due to listening lock.
-    But it does check for the lock on each event release, so if while the loop is running
-    someone elsewhere sets the listening lock, it breaks the loop and returns.
-  */
+  /**
+   * Releases all the events that were previously queued due to listening lock.
+   * 
+   * It checks for the lock on each event release, so if while the loop is running
+   * someone elsewhere sets the listening lock, it breaks the loop and returns.
+   */
   _releaseQueuedEvents() {
     while(!events_queue.isEmpty && listening_lock == false) {
       var e = events_queue.removeAt(0);
@@ -45,6 +72,9 @@ abstract class Subscriber {
     _listening_lock = false;
   }
 
+  /**
+   * Invokes an assigned event handler for the event, passes itself to it. 
+   */
   _handleEvent(e, [data=null]) {
 
     if(event_handlers[e] != null) {
@@ -67,7 +97,6 @@ abstract class Subscriber {
     // That way you can control perfectly what is being propagated. For instance,
     // you can set an event handler for 'DummyPublisher.update' which, in turn, triggers
     // a handler for 'update' event and this last one gets propagated.
-
     if(this is Publisher && !e.contains('.') && event_handlers.keys.contains(e))
       publishEvent(e);
 
